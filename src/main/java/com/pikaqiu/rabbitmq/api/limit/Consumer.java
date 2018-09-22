125c57f4ed96a800f8b383f6e365816773453976
pikaqiu <1047984367@qq.com>
2018-09-22 09:56:36 +0800

    消息队列学习

--- src/main/java/com/pikaqiu/rabbitmq/api/limit/Consumer.java
+++ src/main/java/com/pikaqiu/rabbitmq/api/limit/Consumer.java
@@ -8,41 +8,43 @@ import com.rabbitmq.client.QueueingConsumer.Delivery;
 
 public class Consumer {
 
-	/**
-	 * 限流
-	 * @param args
-	 * @throws Exception
-	 */
+    /**
+     * 限流
+     *
+     * @param args
+     * @throws Exception
+     */
 
-	public static void main(String[] args) throws Exception {
-		
-		
-		ConnectionFactory connectionFactory = new ConnectionFactory();
-		connectionFactory.setHost("134.175.5.236");
-		connectionFactory.setPort(5672);
-		connectionFactory.setVirtualHost("/");
-		
-		Connection connection = connectionFactory.newConnection();
-		Channel channel = connection.createChannel();
-		
-		
-		String exchangeName = "test_qos_exchange";
-		String queueName = "test_qos_queue";
-		String routingKey = "qos.#";
-		
-		channel.exchangeDeclare(exchangeName, "topic", true, false, null);
-		channel.queueDeclare(queueName, true, false, false, null);
-		channel.queueBind(queueName, exchangeName, routingKey);
-		
-		//1 限流方式  第一件事就是 autoAck设置为 false
+    public static void main(String[] args) throws Exception {
 
-		//一定数量的消息未被确认前 不会有新的消息推送过来
 
-		//1大小限制多少m 2最多处理多少消息 3是否全局 true channel级别 false consumer级别
-		channel.basicQos(0, 1, false);
-		
-		channel.basicConsume(queueName, false, new MyConsumer(channel));
-		
-		
-	}
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("134.175.5.236");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/");
+
+        Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+
+
+        String exchangeName = "test_qos_exchange";
+        String queueName = "test_qos_queue";
+        String routingKey = "qos.#";
+
+        channel.exchangeDeclare(exchangeName, "topic", true, false, null);
+        channel.queueDeclare(queueName, true, false, false, null);
+        channel.queueBind(queueName, exchangeName, routingKey);
+
+        //1 限流方式  第一件事就是 autoAck设置为 false
+
+        //一定数量的消息未被确认前 不会有新的消息推送过来
+
+        //1大小限制多少m 0 不限制 2最多处理多少消息 3是否全局 true channel级别 false consumer级别
+        //自动应答下 1 3 不生效
+        channel.basicQos(0, 1, false);
+
+        channel.basicConsume(queueName, false, new MyConsumer(channel));
+
+
+    }
 }
