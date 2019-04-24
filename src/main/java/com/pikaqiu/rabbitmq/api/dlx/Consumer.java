781165fc01c11976f883d28492913f81fff4f868
1047984367@qq.com <as1314520>
2019-04-24 19:36:57 +0800

    修改注释

--- src/main/java/com/pikaqiu/rabbitmq/api/dlx/Consumer.java
+++ src/main/java/com/pikaqiu/rabbitmq/api/dlx/Consumer.java
@@ -34,19 +34,23 @@ public class Consumer {
         //设置死信队列的名称  一般用于消息补偿
         agruments.put("x-dead-letter-exchange", "dlx.exchange");
         //设置整个对上消息的过期时间
-        agruments.put("x-message-ttl", 600000);
+        agruments.put("x-message-ttl", 6000);
 
 		//这个agruments属性，要设置到声明队列上
 		channel.queueDeclare(queueName, true, false, false, agruments);
 		channel.queueBind(queueName, exchangeName, routingKey);
 		
 		//要进行死信队列的声明:  队列死信后会转发到这里
+        //1 没人消费 2消息超时          就会成为死信
 		channel.exchangeDeclare("dlx.exchange", "topic", true, false, null);
 		channel.queueDeclare("dlx.queue", true, false, false, null);
 		channel.queueBind("dlx.queue", "dlx.exchange", "#");
+		//死信后的队列
+		channel.basicConsume("dlx.queue", true, new MyConsumer(channel));
+		//正常队列
+        channel.basicConsume(queueName, true, new MyConsumer(channel));
 
-		channel.basicConsume(queueName, true, new MyConsumer(channel));
-		
-		
-	}
+
+
+    }
 }
