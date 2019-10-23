3c5517cb63dc6997a4b5181219e192acab3f181e
1047984367@qq.com <as1314520>
2019-10-23 10:56:28 +0800

    修改注释

--- src/main/java/com/pikaqiu/rabbitmq/api/ack/Producer.java
+++ src/main/java/com/pikaqiu/rabbitmq/api/ack/Producer.java
@@ -38,8 +38,8 @@ public class Producer {
 					.build();
 			String msg = "Hello RabbitMQ ACK Message " + i;
 			//当mandatory标志位设置为true时，如果exchange根据自身类型和消息routingKey无法找到一个合适的queue存储消息，
-			// 那么broker会调用basic.return方法将消息返还给生产者;当mandatory设置为false时，出现上述情况broker会直接将消息丢弃;通俗的讲，
-			// mandatory标志告诉broker代理服务器至少将消息route到一个队列中，否则就将消息return给发送者;
+			//那么broker会调用basic.return方法将消息返还给生产者;当mandatory设置为false时，出现上述情况broker会直接将消息丢弃;通俗的讲，
+			//mandatory标志告诉broker代理服务器至少将消息route到一个队列中，否则就将消息return给发送者;
 			//想channl 推送消息 交换机名称				mandatory	参数		消息内容字节
 			channel.basicPublish(exchange, routingKey, true, properties, msg.getBytes());
 		}
