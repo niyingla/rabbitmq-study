3ba3a1e2eb5a52d9a4436685b63653ceeffdac05
1047984367@qq.com <as1314520>
2019-10-23 10:46:54 +0800

    修改注释

--- src/main/java/com/pikaqiu/rabbitmq/api/ack/Consumer.java
+++ src/main/java/com/pikaqiu/rabbitmq/api/ack/Consumer.java
@@ -26,7 +26,7 @@ public class Consumer {
 		String routingKey = "ack.#";
 		//定义一个 交换机      交换机名称   交换机类型     是否持久化  是否自动删除   参数
 		channel.exchangeDeclare(exchangeName, "topic", true, false, null);
-		//定义一个qunue 	queue名称	持久化		专有的		自动删除    参数
+		//定义一个queue 		queue名称			持久化		专有的		自动删除    参数
 		channel.queueDeclare(queueName, true, false, false, null);
 		//绑定交换机 队列 和 routingKey的关系
 		channel.queueBind(queueName, exchangeName, routingKey);
